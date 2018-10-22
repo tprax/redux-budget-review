a5bf90860e251c86e0df4817085a2c1e14a4fa2e
tprax <tprax01@gmail.com>
2018-10-22 11:18:01 -0600

    added want list

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Action Creators
 const ADD_ENTRY = 'ADD_ENTRY'
 const REMOVE_ENTRY = 'REMOVE_ENTRY'
+const ADD_ITEM = 'ADD_ITEM'
 
 //Actions
 const addEntry = (entry) => {
@@ -8,16 +9,30 @@ const addEntry = (entry) => {
 }
 
 const removeEntry = (index) => {
-  return { type: REMOVE_ENTRY, index}
+  return { type: REMOVE_ENTRY, index }
+}
+
+const addItem = (item) => {
+  return { type: ADD_ITEM, item }
 }
 
 //Reducer
+
+const items = ( state = [], action ) => {
+  switch(action.type) {
+    case ADD_ITEM:
+      return [...state, action.item]
+    default:
+      return state
+  }
+}
+
 const ledger = (state = [], action) => {
   switch(action.type) {
     case ADD_ENTRY:
       return [...state, action.entry]
     case REMOVE_ENTRY:
-      return state.filter((_, i) => i !== action.index)
+      return state.filter( (_, i) => i !== action.index )
     default:
       return state
   }
@@ -33,6 +48,7 @@ const { createStore, combineReducers, compose } = Redux
 
 const rootReducer = combineReducers({
   ledger,
+  items,
 })
 
 const store = createStore(
@@ -44,13 +60,14 @@ const store = createStore(
 const sumEntries = () => {
   const h1 = document.getElementById('total')
   h1.innerHTML = null
-  const {ledger} = store.getState()
-  const value = ledger.reduce((total, entry) => {
+  const { ledger } = store.getState()
+  const value = ledger.reduce( (total, entry) => {
     const amt = parseFloat(entry.amt)
     if (entry.type === 'Credit')
       return total + amt
     return total - amt
   }, 0)
+
   h1.innerHTML = `$${value}`
 }
 
@@ -63,7 +80,7 @@ const updateHistory = () => {
     const span = document.createElement('span')
     const button = document.createElement('button')
     span.innerHTML = `$${entry.amt} - ${entry.description}`
-    button.addEventListener('click',() => store.dispatch(removeEntry(index)))
+    button.addEventListener('click', () => store.dispatch(removeEntry(index)) )
     button.innerText = 'Remove'
     span.appendChild(button)
     item.appendChild(span)
@@ -72,25 +89,52 @@ const updateHistory = () => {
   })
 }
 
-const handleSubmit = (e) => {
-  e.preventDefault()
+const updateItems = () => {
+  const list = document.getElementById('items')
+  list.innerHTML = null
+  const { items } = store.getState()
+  items.forEach( (item) => {
+    const li = document.createElement('li')
+    li.innerHTML = `$${item.cost} - ${item.description}`
+    list.appendChild(li)
+  })
+}
+
+const formElements = (form) => {
   const obj = {}
-  const form = e.target
   for (let el of form.elements) {
     if (el.name)
       obj[el.name] = el.value
   }
 
+  return obj
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const form = e.target
+  const obj = formElements(form)
+
   store.dispatch(addEntry(obj))
   form.reset()
 }
 
+const handleItemForm = (e) => {
+  e.preventDefault()
+  const form = e.target
+  const obj = formElements(form)
+  store.dispatch(addItem(obj))
+  form.reset()
+}
+
 const log = () => {
-  console.log( store.getState().ledger )
+  console.log( store.getState() )
 }
 
 store.subscribe(updateHistory)
 store.subscribe(sumEntries)
 store.subscribe(log)
+store.subscribe(updateItems)
 
 document.getElementById('add_entry').addEventListener('submit', handleSubmit)
+document.getElementById('add_item').addEventListener('submit', handleItemForm)
